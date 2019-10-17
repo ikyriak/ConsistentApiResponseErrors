4bc04469d6b690959fb04f94bb252bfe5d8f5ea3
Ioannis Kyriakidis <little_john85@hotmail.com>
2019-10-17 18:29:17 +0300

    Add error class as separated file

--- ConsistentApiResponseErrors/ConsistentErrors/ValidationError.cs
+++ ConsistentApiResponseErrors/ConsistentErrors/ValidationError.cs
@@ -27,37 +27,6 @@ namespace ConsistentApiResponseErrors.ConsistentErrors
         public string StatusMessage { get; set; }
         public string TraceId { get; set; }
         public List<Error> Errors { get; set; }
-
-
-        [Serializable]
-        public class Error
-        {
-            public Error()
-            {
-                Code = string.Empty;
-                Field = string.Empty;
-                AttemptedValue = null;
-                Message = string.Empty;
-                HelpURL = string.Empty;
-            }
-
-            public Error(ValidationFailure validationFailure)
-            {
-                Code = validationFailure.ErrorCode;
-                Field = validationFailure.PropertyName;
-                AttemptedValue = validationFailure.AttemptedValue;
-                Message = validationFailure.ErrorMessage;
-
-                // TODO: To be implemented!
-                HelpURL = string.Empty;
-            }
-
-            public string Code { get; set; }
-            public string Field { get; set; }
-            public object AttemptedValue { get; set; }
-            public string Message { get; set; }
-            public string HelpURL { get; set; }
-        }
     }
 
 }
